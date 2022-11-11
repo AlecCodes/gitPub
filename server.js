97a615dbcd7d50ac9e7b0e64a2449355b7584611
Alec Hannaford <alec322@gmail.com>
2022-11-10 21:08:23 -0800

    added get route to render json drinks data

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express();
+const drinks = require('./models/drinks')
 
+//GET ROUTE
+app.get('/drinks', (req, res) =>{
+    res.send(drinks)
+})
 
 
 //Listeners
